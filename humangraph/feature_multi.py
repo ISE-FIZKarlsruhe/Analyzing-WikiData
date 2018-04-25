a3da454ef3a665cf8ac4a7ae99b2a95b865b7f64
Philipp <pskudlik@gmail.com>
2018-04-25 20:44:05 +0200

    16 cores

--- humangraph/feature_multi.py
+++ humangraph/feature_multi.py
@@ -28,7 +28,7 @@ def main():
     edgelist = pd.read_csv(inputfile, sep=";", header=None)
     threshold = 10
 
-    pool = Pool(4)
+    pool = Pool(16)
     pool.map(functools.partial(getConnectedPersons, auxdict=auxdict, edgelist=edgelist, threshold=threshold), G1.items())
     pool.close()
     pool.join()
