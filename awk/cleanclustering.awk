b11bd04d8b9d7cc6e219e88816a5a2efd43e4140
Philipp <pskudlik@gmail.com>
2018-04-28 23:45:36 +0200

    clustering analysis piepline

--- awk/cleanclustering.awk
+++ awk/cleanclustering.awk
@@ -1,5 +1,6 @@
 match($0, /'(.*)'/, a) {
 o = a[1]; 
+sub(" Members: \\[",";",o);
 gsub("'","",o);
 gsub(", ",";",o);
 print o}
--- graphcluster/analyseclusters.py
+++ graphcluster/analyseclusters.py
@@ -18,7 +18,7 @@ inputfile_labels = "../results/labels_english"
 parser = argparse.ArgumentParser()
 parser.add_argument('cluster_file', type=str, nargs='?', help="specifies the cluster inputfile. Must be .csv with a list of clustermembers in each row.", default=inputfile_clusters, action="store")
 parser.add_argument('edgelist_file', type=str, nargs='?', help="specifies the edgelist inputfile. Must be a two column .csv containing specific entities on the left and their 'objects' on the right.", default=inputfile_edgelist, action="store")
-parser.add_argument('labels_file', type=str, nargs='?', help="specifies the label inputfile. Must be a two column .csv containing specific entities on the left and their 'objects' on the right.", default=inputfile_labels, action="store")
+parser.add_argument('labels_file', type=str, nargs='?', help="specifies the label inputfile. Must be a two column .txt. Label ist seperated from reference number by space.", default=inputfile_labels, action="store")
 parser.add_argument("--verbose", "-v", help="increase output verbosity", action="store_true")
 args = parser.parse_args()
 inputfile_clusters = args.cluster_file
@@ -40,12 +40,6 @@ def main():
             number, text = line.split(' ', maxsplit=1)
             text = text[:-1]
             labeldict[number] = text
-        #labeldict = dict([line.split(' ', maxsplit=1) for line in labelfile])
-
-
-    #labels = pd.DataFrame.from_records(data, columns=['Integer', 'String'])
-    #labels = pd.read_csv(inputfile_labels, sep=labelfile_delimiter, header=None, engine='python')
-
 
     with open(inputfile_clusters) as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
@@ -53,33 +47,19 @@ def main():
         for line in reader:
             linenumber += 1
             attcount = defaultdict(int)
-            cluster = np.array(line)
+            size = line[0]
+            cluster = np.array(line[1:])
             all_attributes = np.array(edgelist.loc[edgelist[0].isin(cluster)][1].values)
             for e in all_attributes:
                 attcount[e] += 1
-
             sorted_attcount = sorted(attcount.items(), key=operator.itemgetter(1), reverse=True)
 
-            #old approach
-            # print("\nCluster %d: Attributes:"%(linenumber),end=" ")
-            # freq_attribute = []
-            # freq_attribute_numbers = []
-            # for attribute, count in sorted_attcount[:5]:
-            #     freq_attribute.append(attribute)
-            #     freq_attribute_numbers.append(tuple((attribute, str("%.2f" % round(count / len(cluster)*100,2)+"%"))))
-            # print(freq_attribute_numbers)
-            # names = labels.loc[labels[0].isin(freq_attribute)][1].tolist()
-            # print("Cluster %d: Attributes:" % (linenumber), end=" ")
-            # print(names)
-            # if len(names) < len(freq_attribute):
-            #     print(len(freq_attribute)-len(names)," label(s) not found!")
-
-            print("\nCluster %d:"%(linenumber))
+            print("\nCluster No. %d: Size %d"%(linenumber,int(size)))
             freq_attribute = []
-            for attribute, count in sorted_attcount[:10]:
+            for attribute, count in sorted_attcount[:15]:
                 #label = labeldict[str(attribute)] if str(attribute) in labeldict else "no label"
                 label = labeldict.get(str(attribute))
-                freq_attribute.append([attribute, label, str("%.2f" % round(count / len(cluster)*100,2)+"%")])
+                freq_attribute.append([label, attribute, str("%.2f" % round(count / len(cluster)*100,2)+"%")])
             for f in freq_attribute:
                 print(f)
 
