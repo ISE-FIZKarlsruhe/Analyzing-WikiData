94b43a73e50b022de162b4742b3568ac4794da71
Philipp <pskudlik@gmail.com>
2018-05-03 15:34:29 +0200

    shell pipeline

---
+++ graphcluster/clusterfinding_server.sh
@@ -0,0 +1,20 @@
+awkscripts="$1"
+humanwhoscript="$awkscripts""humangraphwho2.awk"
+wikidata="$2smallgraph7_undirected.txt"
+pythonscripts="$3"
+overlapgraphscript="$pythonscripts""overlapgraph.py"
+
+criterion=30
+
+threshold=4
+poolsize=16
+
+#echo $wikidata
+#echo $humanwhoscript
+#echo $criterion
+
+removetrivial='BEGIN{FS=";"}{if($2!=5&&$2!=6581097&&$2!=6581072&&$2!='$criterion')print$0}'
+awk -v c=$criterion -f $humanwhoscript $wikidata | awk $removetrivial > criteriongraph.csv
+python -u $overlapgraphscript -w -t $threshold -p $poolsize criteriongraph.csv | 
+grep ";" | sort -V  
+
