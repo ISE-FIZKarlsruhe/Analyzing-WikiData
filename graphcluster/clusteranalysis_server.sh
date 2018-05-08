dde1f2a3f1aa3a4ed269a8e94412db64c10d9c53
Philipp <pskudlik@gmail.com>
2018-05-08 13:07:54 +0200

    pipe

--- graphcluster/clusteranalysis_server.sh
+++ graphcluster/clusteranalysis_server.sh
@@ -5,6 +5,7 @@ pythonscripts="$3"
 analyseclustersscript="$pythonscripts""analyseclusters.py"
 labels="$4"
 
+#
 awk -f $cleanclustering $louvainoutput | 
 sort -r -V -t ";" -k 1  > louvain_clean
 python -u $analyseclustersscript louvain_clean criteriongraph.csv labels
