73a89d22f8588f5ac117aac116a7f17542ba6c8a
Philipp <pskudlik@gmail.com>
2018-05-28 00:30:05 +0200

    induced graph

--- graphcluster/clusterinduced_graph_server.sh
+++ graphcluster/clusterinduced_graph_server.sh
@@ -4,4 +4,4 @@ inputfile="$2" #louvainoutput unclean
 clusternumber="$3"
 overlapgraph="$4"
 grep "Partition $clusternumber:" $inputfile | awk -f $printclutermembers > $clusternumber"Member"
-cat <(join -1 1 -2 1 -t';' -o 1.1,1.2,1.3 <(sort -k1 -t';' $overlapgraph) <(sort $clusternumber"Member")) <(join -1 2 -2 1 -t';' -o 1.1,1.2,1.3 <(sort -k2 -t';' $overlapgraph) <(sort $clusternumber"Member")) | uniq > $clusternumber"induced_graph"
+cat <(LOCALE=C join -1 1 -2 1 -t';' -o 1.1,1.2,1.3 <(LOCALE=C sort -k1 -t';' $overlapgraph) <(LOCALE=C sort $clusternumber"Member")) <(LOCALE=C join -1 2 -2 1 -t';' -o 1.1,1.2,1.3 <(LOCALE=C sort -k2 -t';' $overlapgraph) <(LOCALE=C sort $clusternumber"Member")) | uniq > $clusternumber"induced_graph"
