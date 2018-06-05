391c81813fc4e5e0871e4fcb28f8c096f94ce8b9
Philipp <pskudlik@gmail.com>
2018-06-05 14:32:07 +0200

    analyse with params

--- graphcluster/analyseclusters2.py
+++ graphcluster/analyseclusters2.py
@@ -35,13 +35,13 @@ def main():
     print("Loading edgelist...")
     edgelist = pd.read_csv(inputfile_edgelist, sep=";", header=None)
 
-    print("Building label dictionary...")
-    labeldict = defaultdict()
-    with open(inputfile_labels, encoding="utf8") as labelfile:
-        for line in labelfile:
-            number, text = line.split(' ', maxsplit=1)
-            text = text[:-1]
-            labeldict[number] = text
+    # print("Building label dictionary...")
+    # labeldict = defaultdict()
+    # with open(inputfile_labels, encoding="utf8") as labelfile:
+    #     for line in labelfile:
+    #         number, text = line.split(' ', maxsplit=1)
+    #         text = text[:-1]
+    #         labeldict[number] = text
 
     with open(inputfile_clusters) as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
@@ -49,9 +49,10 @@ def main():
         cluster_threshold = args.threshold
         attributes_aggregated = defaultdict(float)
 
-        clustering = defaultdict(list)
+        candidates = defaultdict(list)
         sizes = defaultdict(int)
         description = defaultdict(list)
+        nonparticipatings = defaultdict(float)
 
         #Iterate over each line containg one Cluster.
         #Genereate a candidate attributeset for every Cluster/Line
@@ -61,42 +62,62 @@ def main():
             cluster_id = int(line[0])
             cluster_size = int(line[1])
             cluster_members = np.array(line[2:])
-            
+
             if int(cluster_size) > cluster_threshold:
+                #get the attributes (i.e. the objects all members of the cluster link to)
                 sizes[cluster_id] = cluster_size
                 all_attributes = np.array(edgelist.loc[edgelist[0].isin(cluster_members)][1].values)
+                #rank all alltributes of this cluster by frequency
                 for e in all_attributes:
                     attcount[e] += 1
-                sorted_attcount = sorted(attcount.items(), key=operator.itemgetter(1), reverse=True)
+                sorted_attcount = sorted(attcount.items(), key=operator.itemgetter(1), reverse=True)[:25]
 
-                #print("\nCluster No. %d: Size %d"%(cluster_id,int(cluster_size)))
-                for attribute, count in sorted_attcount[:25]:
-                    #label = labeldict[str(attribute)] if str(attribute) in labeldict else "no label"
-                    #label = labeldict.get(str(attribute))
-                    clustering[cluster_id].append((int(attribute), round(count / len(cluster_members)*100,2)))
+                #append the 25 most frequent attributes to the current clusters candidates dictionary (i.e. the clustering)
+                for attribute, count in sorted_attcount:
+                    candidates[cluster_id].append((int(attribute), round(count / len(cluster_members)*100,2)))
+                    #save the aggregated (sum) information on the attribute frequency in a seperate dict
                     attributes_aggregated[int(attribute)] += round(count / len(cluster_members)*100,2)
 
-        n = len(clustering.keys())   #how many clusters are there
+                #Calculate percentage of Members of this cluster who do not link to any of the top 25 'attributes'
+                nonparticipating = 0
+                for member in cluster_members:
+                    thismembers_attributes = np.array(edgelist.loc[edgelist[0]==int(member)][1].values)
+                    intersect_with_frequent=np.intersect1d(thismembers_attributes,sorted_attcount)
+                    if not intersect_with_frequent.any():
+                        nonparticipating += 1
+                nonparticipatings[cluster_id] = round(nonparticipating/cluster_size,5)
 
-        for this_cluster in clustering.items():
+
+        #get the number of clusters with a number of members above the threshold
+        n = len(candidates.keys())   #how many clusters are there
+
+        #Generate the calculated information for each candidate attribute and sord the candidate attributes by their score value
+        for this_cluster in candidates.items():
             for candidate in this_cluster[1]:
                 unique = 1 if attributes_aggregated[int(candidate[0])]-candidate[1]==0 else 0
-                description[this_cluster[0]].append((candidate[0], labeldict.get(str(candidate[0])), candidate[1], candidate[1] - ((attributes_aggregated[int(candidate[0])]-candidate[1])/(n-1)),unique))
+                #description[this_cluster[0]].append((candidate[0], labeldict.get(str(candidate[0])), candidate[1], round(candidate[1] - ((attributes_aggregated[int(candidate[0])]-candidate[1])/(n-1)),2),unique))
+                description[this_cluster[0]].append((candidate[0], candidate[1], round(candidate[1] - ((attributes_aggregated[int(candidate[0])]-candidate[1])/(n-1)),2),unique))
+
             description[this_cluster[0]].sort(key=lambda tup: tup[3], reverse=True)
 
-
+        #Converting and sorting the size values to easy interpretation and correct iteration
+        sizes_df = pd.Series(list(sizes.values()))
         sorted_sizes = sorted(sizes.items(), key=operator.itemgetter(1),reverse=True)
 
+###PRINTING RESULTS
+        print("\n_____________________\nClustering Metainformation:")
+        #print(sizes_df.describe()[['count','mean','std']].reset_index().to_csv(header=None, index=None, sep=' '))
+        print("count %f" % (int(sizes_df.count())))
+        print("size mean %f" % (int(sizes_df.mean())))
+        print("size std %f" % (int(sizes_df.std())))
+        print("size variance %f" %(int(sizes_df.var())))
+        print("avg nonpart %f" % (np.array(list(nonparticipatings.values())).sum()/sizes_df.count()))
+
+        print("\n_____________________\nSpecific Cluster ordered by size:")
         for id,size in sorted_sizes:
-            print("\nCluster No.", id, " Members:", size)
+            print("\nCluster No. %d Members: %d Nonparticipating: %f" %(id, size, nonparticipatings[id]))
             for attribute in description[id]:
                 print(attribute)
-
-        # for cluster in description.items():
-        #     print("\nCluster No.",cluster[0], " Members:", sizes[cluster[0]])
-        #     for attribute in cluster[1]:
-        #         print(attribute)
-
 
 
 
