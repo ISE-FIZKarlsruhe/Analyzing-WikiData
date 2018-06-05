4b6daade0a41e521c1d73d67da24a65b6d55bab6
Philipp <pskudlik@gmail.com>
2018-06-05 12:04:19 +0200

    analyse with params

--- graphcluster/clusteranalysis_server.sh
+++ graphcluster/clusteranalysis_server.sh
@@ -6,7 +6,7 @@ analyseclustersscript="$pythonscripts""analyseclusters2.py"
 labels="$4"
 
 #
-awk -f $cleanclustering2 $louvainoutput | 
+awk -f $cleanclustering $louvainoutput | 
 sort -r -V -t ";" -k 2  > louvain_clean_names
 python -u $analyseclustersscript louvain_clean_names criteriongraph.csv $labels
  
