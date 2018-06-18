a48774968167d718efe6a092e7e80530c296590d
Philipp <pskudlik@gmail.com>
2018-06-18 22:33:14 +0200

    3 colums criteriongraph

--- graphcluster/clusterfinding_server.sh
+++ graphcluster/clusterfinding_server.sh
@@ -1,5 +1,5 @@
 awkscripts="$1"
-humanwhoscript="$awkscripts""humangraphwho3.awk"
+humanwhoscript="$awkscripts""humangraphwho4.awk"
 wikidata="$2"
 pythonscripts="$3"
 overlapgraphscript="$pythonscripts""overlapgraph.py"
@@ -8,7 +8,7 @@ threshold="$5"
 
 poolsize=16
 
-removetrivial='BEGIN{FS=";"}{if($2!=5&&$2!='$criterion')print$0}'
+removetrivial='BEGIN{FS=";"}{if($3!=5&&$3!='$criterion')print$0}'
 awk -v c=$criterion -f $humanwhoscript $wikidata | awk $removetrivial | awk '!s[$0]++' > criteriongraph.csv
 python -u $overlapgraphscript -w -t $threshold -p $poolsize criteriongraph.csv
 
