a766cbea5fee540afdf36d6366aaa3916222f1f7
Philipp <pskudlik@gmail.com>
2018-06-19 20:57:03 +0200

    pagerank and predicates

--- graphcluster/clusteranalysis_server.sh
+++ graphcluster/clusteranalysis_server.sh
@@ -4,9 +4,10 @@ cleanclustering="$awkscripts""cleanclustering2.awk"
 pythonscripts="$3"
 analyseclustersscript="$pythonscripts""analyseclusters3.py"
 labels="$4"
+overlapgraph="overlapgraph_clean"
 
 #
 awk -f $cleanclustering $louvainoutput | 
 sort -r -V -t ";" -k 2  > louvain_clean_names
-python -u $analyseclustersscript louvain_clean_names criteriongraph.csv $labels
+python -u $analyseclustersscript louvain_clean_names criteriongraph.csv $labels $overlapgraph
  
