f2ab86163fb78a0efbcee1315506b6c5254e7f34
Philipp <pskudlik@gmail.com>
2018-06-19 20:49:44 +0200

    pagerank and predicates

--- graphcluster/clusteranalysis_server.sh
+++ graphcluster/clusteranalysis_server.sh
@@ -2,7 +2,7 @@ louvainoutput="$1"
 awkscripts="$2"
 cleanclustering="$awkscripts""cleanclustering2.awk"
 pythonscripts="$3"
-analyseclustersscript="$pythonscripts""analyseclusters2.py"
+analyseclustersscript="$pythonscripts""analyseclusters3.py"
 labels="$4"
 
 #
