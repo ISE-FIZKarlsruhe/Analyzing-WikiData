ea8e88b1770519c863cfb9267322d95af937ed9c
Philipp <pskudlik@gmail.com>
2018-06-21 18:08:39 +0200

    supertype enroll

---
+++ graphcluster/supertype_enroll.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+import numpy as np
+import csv
+from collections import defaultdict
+import argparse
+import pandas as pd
+import operator
+import networkx as nx
+from collections import Counter
+
+
+####File parameter
+#----------------------------------------------------------------
+inputfile_supertypegraph = "../results/wikidata_supertypeGraph_noqual.csv"
+inputfile_labels = "../results/labels_english"
+parser = argparse.ArgumentParser()
+parser.add_argument('supertype_file', type=str, nargs='?', help="specifies the cluster inputfile. Must be .csv with a list of clustermembers in each row.", default=inputfile_supertypegraph, action="store")
+parser.add_argument('labels_file', type=str, nargs='?', help="specifies the label inputfile. Must be a two column .txt. Label ist seperated from reference number by space.", default=inputfile_labels, action="store")
+parser.add_argument('concept', type=int, nargs='?', help="specifies the concept all printet entities will belong to.", default=4830453, action="store")
+args = parser.parse_args()
+inputfile_supertypegraph = args.supertype_file
+inputfile_labels = args.labels_file
+
+start = args.concept
+
+#inputfile = "../py/smallgraph8.csv"
+#start = 7
+
+i = 0
+
+def main():
+    printLowestLevel(start)
+    #print("------")
+    #print(i)
+
+def printLowestLevel(node):
+    global i
+    if Graph.get(node) == None:
+        print(node)
+        i +=1
+        return
+    for subtype in Graph.get(node):
+        printLowestLevel(subtype)
+
+
+
+def printAllLevels(node, level):
+    if Graph.get(node) == None:
+        return
+    for subtype in Graph.get(node):
+        print("---"*level,subtype,Labels.get(str(subtype)))
+        printAllLevels(subtype, level+1)
+
+
+
+def buildLabelDict(inputfile):
+    labeldict = defaultdict()
+    #print("Building up labeldictionary", end=" ")
+    with open(inputfile, encoding="utf8") as labelfile:
+        for line in labelfile:
+            number, text = line.split(' ', maxsplit=1)
+            text = text[:-1]
+            labeldict[number] = text
+    #print("\nDictionary built successfully!\n")
+    return labeldict
+
+
+def buildReversed(inputfile):
+    dictionary = defaultdict(list);
+    i = 0;
+    #print("Building up dictionary", end=" ")
+    with open(inputfile, newline ='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+
+        for key, value in reader:
+            dictionary[int(value)].append(int(key))
+    #print("\nDictionary built successfully!\n")
+    return dictionary
+
+Graph = buildReversed(inputfile_supertypegraph)
+Labels = buildLabelDict(inputfile_labels)
+main()
